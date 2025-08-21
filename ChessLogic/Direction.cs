cf0d3281c3b2e07d6d5b91f899368b3d07ed00cd
= <=>
2025-08-21 14:00:45 +0300

    Add project files.

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Direction
+    {
+
+        public readonly static Direction North = new Direction(-1, 0);
+        public readonly static Direction West = new Direction(0, -1);
+        public readonly static Direction South = new Direction(1, 0);
+        public readonly static Direction East = new Direction(0, 1);
+        public readonly static Direction NorthEast = North + East;
+        public readonly static Direction NorthWest = North + West;
+        public readonly static Direction SouthEast = South + East;
+        public readonly static Direction SouthWest = South + West;
+
+
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            this.RowDelta = rowDelta;
+            this.ColumnDelta = columnDelta;
+        }
+
+
+        // calculates the new direction sum
+        public static Direction operator +(Direction a, Direction b)
+        {
+            return new Direction(a.RowDelta + b.RowDelta, a.ColumnDelta + b.ColumnDelta);
+        }
+
+
+        // uses vector calculation of direction with a scalar
+        public static Direction operator *(int scalar,Direction dir1)
+        {
+            return new Direction(dir1.RowDelta * scalar, dir1.ColumnDelta * scalar);
+        }
+
+
+
+    }
+}
---
+++ ChessLogic/Postion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Postion
+    {
+        public int row { get; }
+        public int column { get; }
+
+
+        // constructor
+        public Postion(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+            
+        /*
+         * function to check the square color by cheking if the sum of the row and column is even or odd
+         * input: none
+         * output: the sqaure color white or black (1 or 2)
+        */
+        public Player SquareColor()
+        {
+            return (row + column) % 2 == 0 ? Player.White : Player.Black;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Postion postion &&
+                   row == postion.row &&
+                   column == postion.column;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(row, column);
+        }
+
+        public static bool operator ==(Postion left, Postion right)
+        {
+            return EqualityComparer<Postion>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Postion left, Postion right)
+        {
+            return !(left == right);
+        }
+    }
+}
